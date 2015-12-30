268307fc90c8d5bb19b579c4c6dcce062ff37bd7
Paul Cronk <paul.cronk@digital.cabinet-office.gov.uk>
2015-12-30 15:32:29 +0000

    Updates and editors to main script

--- search-console/console-query-position.py
+++ search-console/console-query-position.py
@@ -17,20 +17,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Example for using the Google Search Analytics API (part of Search Console API).
-A basic python command-line example that uses the searchAnalytics.query method
-of the Google Search Console API. This example demonstrates how to query Google
-search results data for your property. Learn more at
-https://developers.google.com/webmaster-tools/
-To use:
-1) Install the Google Python client library, as shown at https://developers.google.com/webmaster-tools/v3/libraries.
-2) Sign up for a new project in the Google APIs console at https://code.google.com/apis/console.
-3) Register the project to use OAuth2.0 for installed applications.
-4) Copy your client ID, client secret, and redirect URL into the client_secrets.json file included in this package.
-5) Run the app in the command-line as shown below.
-Sample usage:
-  $ python search_analytics_api_sample.py 'https://www.gov.uk/' '2015-11-01' '2015-11-30'
-"""
 
 import argparse
 import sys
@@ -48,8 +34,9 @@ argparser.add_argument('start_date', type=str,
 argparser.add_argument('end_date', type=str,
                        help=('End date of the requested date range in '
                              'YYYY-MM-DD format.'))
-#argparser.add_argument('keyword', type=str,
-#                       help=('keyword being searched for.'))
+
+# an example of the command line might be:
+# $ python search_analytics_api_sample.py 'https://www.gov.uk/' '2015-11-01' '2015-11-30'
 
 keyword = raw_input('Enter the search term (or leave it blank for all searches): ')
 
@@ -63,9 +50,12 @@ def main(argv):
       scope='https://www.googleapis.com/auth/webmasters.readonly')
 
 
-
   # For a given landing page, which search terms bring traffic to that page (impressions, clicks and CTR)
   # and what position does the page appear? How does this change over time?
+
+  # This is set up to request three dimensions (date, query/keyword, landing page) and
+  # is filtered by keyword and landing page. The filter is 'contains'.
+
   request = {
       'startDate': flags.start_date,
       'endDate': flags.end_date,
@@ -109,12 +99,11 @@ def new_file(response):
         temp_list = []
         if 'keys' in row:
             keys = u','.join(row['keys']).encode('utf-8')
-        # print '{0},{1},{2},{3},{4}'.format(keys,row['clicks'],row['impressions'],row['ctr'],row['position'])
         temp_list = (keys,row['clicks'],row['impressions'],row['ctr'],row['position'])
         new_output.append(temp_list)
     print new_output
     new_output = pd.DataFrame(new_output)
-    new_output.columns = ['date,query', 'clicks', 'impressions', 'ctr', 'position']
+    new_output.columns = ['date,query,url', 'clicks', 'impressions', 'ctr', 'position']
     new_output.to_csv('output.csv', sep=',')
 
 if __name__ == '__main__':
